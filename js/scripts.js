4b25544b7ff694397e81225673e3974d59ab30e9
Blankphrase <ckasera6486@gmail.com>
2018-05-13 21:52:07 +0300

    This commit is to show all the buttons are fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,50 @@
 // Business logic
 
-var player = "";
-var opponent = "";
-
-var Entries = function (signee) {
-  this.signee = signee;
-}
-
-var diceGamble = function () {
-  return Math.floor(Math.random()) + 1;
-}
-
-var rollDice = function(roll, score, total) {
+function rollDice(roll, score, total) {
   this.roll = roll;
   this.score = score;
   this.total = total;
 }
 
-rollDice.prototype.play = function () {
+var player = new rollDice(0, 0, 0);
+var opponent = new rollDice(0, 0, 0);
+
+rollDice.prototype.play = function() {
   if (this.roll === 1) {
     this.score = 0;
-    alert ("Sorry loser, back off")
+    alert ("Sorry, better luck next time")
   } else {
     this.score += this.roll;
     alert ("We are on a roll")
   }
 }
 
-rollDice.prototype.hold = function () {
+rollDice.prototype.hold = function() {
   this.total += this.score;
   this.score = 0;
   alert ("Quitter!!, that's right")
 }
 
-rollDice.prototype.finale = function () {
+rollDice.prototype.finale = function() {
   if (this.total >= 100) {
     alert ("Hurray!!! " + this.signee + " the Crusher")
   }
+}
+
+rollDice.prototype.restart = function(roll, score, total) {
+  this.roll = roll;
+  this.score = score;
+  this.total = total;
+}
+
+rollDice.prototype.game = function(roll, score, total) {
+  this.roll = roll;
+  this.score = score;
+  this.total = total;
+}
+
+var diceGamble = function() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
 // user interface logic
@@ -49,12 +57,16 @@ $(document).ready(function() {
     $("span#playerOutput").text(firstPlayer);
     $("span#opponentOutput").text(secondPlayer);
   });
+
+
   $("button#playerTurn").click(function(event) {
     player.roll = diceGamble();
     $("diceRoll").text(player.roll);
     player.play();
     $("#score").text(player.score);
   });
+
+
   $("button#playerPause").click(function(event) {
     player.hold();
     $("#total").text(player.total);
@@ -62,11 +74,47 @@ $(document).ready(function() {
     $("#diceRoll").empty();
     player.finale();
   });
+
+
   $("button#opponentTurn").click(function(event) {
     opponent.roll = diceGamble();
     $("#diceRoll2").text(opponent.roll);
     opponent.play();
     $("#score2").text(opponent.score);
   });
-  $("button#opponentPause").click(function())
-})
+
+
+  $("button#opponentPause").click(function(event) {
+    opponent.hold();
+    $("#total2").text(opponent.total);
+    $("#score2").empty();
+    $("#diceRoll2").empty();
+    opponent.finale();
+  });
+
+
+  $("button#restart").click(function() {
+    $("input#player").val("");
+    $("input#opponent").val("");
+    player.restart();
+    opponent.restart();
+    $("#diceRoll").empty();
+    $("#diceRoll2").empty();
+    $("#score").empty();
+    $("#score2").empty();
+    $("#total").empty();
+    $("#total2").empty();
+  });
+  $("button#restart").click(function() {
+    $("input#player").empty();
+    $("input#opponent").empty();
+    player.game();
+    opponent.game();
+    $("#diceRoll").empty();
+    $("#diceRoll2").empty();
+    $("#score").empty();
+    $("#score2").empty();
+    $("#total").empty();
+    $("#total2").empty();
+  });
+});
